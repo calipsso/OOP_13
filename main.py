f2b764e8449d5aca86e483b9fd64132d428f918b
calipsso <kamil.vizy@gmail.com>
2024-02-13 20:30:54 +0100

    Abstraktna metoda

---
+++ main.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+class Zviera(ABC):
+    @abstractmethod
+    def zvuk(self):
+        pass
+class Pes(Zviera):
+    def zvuk(self):
+        return "haf"
+class Macka(Zviera):
+    def zvuk(self):
+        return "Mnau"
+
+moj_pes = Pes()
+moja_macka = Macka()
+
+print(moj_pes.zvuk())
+print(moja_macka.zvuk())
